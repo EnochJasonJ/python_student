c3f1717377085d557f43017a3157f8f7a9da74d1
Enoch Jason <enochjason@Enochs-MacBook-Air.local>
2024-08-20 09:52:34 +0530

    UPdated on Tuesday 20th August 2024 9:52 AM.

---
+++ student.py
@@ -0,0 +1,13 @@
+Testcase1 = [101,0,102,0,10,7,0,0]
+index = 0
+
+for i in range(len(Testcase1)):
+    if Testcase1[i] != 0 :
+        Testcase1[index] = Testcase1[i]
+        index +=1
+
+while index < len(Testcase1):
+    Testcase1[index] = 0
+    index += 1
+
+print(Testcase1)
